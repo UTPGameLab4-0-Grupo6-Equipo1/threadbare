2015685db5876bf1ff55ca258706f886b70e935d
Gian Piero Cano <154576915+GianPierooo@users.noreply.github.com>
2025-07-07 16:12:16 -0500

    Delete scenes/quests/story_quests/shjourney directory

--- scenes/quests/story_quests/shjourney/3_shjourney_intro/area_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_Area2D_body_entered(body: Node) -> void:
-	if body.name == "Player":
-		print("¡Jugador detectado!")
-		$"../Cinematic_siguiente".start_cinematic()
--- scenes/quests/story_quests/shjourney/4_Laberinto/ScripBase.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _on_timer_puerta_timeout() -> void:
-	$"CamaraPuerta".enabled = false
-	$"Player/Camera2D".enabled = true
